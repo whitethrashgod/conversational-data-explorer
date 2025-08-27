51b194169bd0f1b6871c371837f85809803e32c9
whitethrashgod <138958161+whitethrashgod@users.noreply.github.com>
2025-08-27 19:55:53 +0530

    Update Results.tsx

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 export default function Results({ rows }: { rows: Record<string, any>[] }) {
